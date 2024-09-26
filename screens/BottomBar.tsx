308152328c9ec6bae55884e1387ad0394b9acb6d
marywam <maryannewambui0124@gmail.com>
2024-09-26 17:09:31 +0300

    made some changes

--- screens/BottomBar.tsx
+++ screens/BottomBar.tsx
@@ -18,38 +18,39 @@ const BottomBar = () => {
 
   return (
     <LinearGradient
-      colors={['#4CAF50', '#2196F3']} // Green to a mixture of blue
+      colors={['white', 'orange', 'white']} // Green to a mixture of blue
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 0 }}
       style={homeStyles.bottombar}>
       
       <View style={homeStyles.shops}>
         <TouchableOpacity onPress={() => handleMenuItem('Home', 'Home')}>
-          <Icon name="water-outline" size={30} color={selectedMenu === 'Home' ? "#fff" : "#000"} />
+          <Icon name="home" size={29} color={selectedMenu === 'Home' ? "#fff" : "#000"} />
         </TouchableOpacity>
-        <Text style={[homeStyles.shops1, { color: selectedMenu === 'Home' ? '#fff' : '#000' }]}>Shops</Text>
+        <Text style={[homeStyles.shops1, { color: selectedMenu === 'Home' ? '#fff' : '#000' }]}>Home</Text>
       </View>
 
       <View style={homeStyles.tokenimage}>
         <TouchableOpacity onPress={() => handleMenuItem('Token', 'Token')}>
-          <View>
-            <Icon name="card-outline" size={30} color={selectedMenu === 'Token' ? '#fff' : '#000'} />
-          </View>
+        <View>
+  <Icon name="heart" size={30} color={selectedMenu === 'Token' ? '#fff' : '#000'} />
+</View>
+
         </TouchableOpacity>
-        <Text style={[homeStyles.tokenimage1, { color: selectedMenu === 'Token' ? '#fff' : '#000' }]}>Reward</Text>
+        <Text style={[homeStyles.tokenimage1, { color: selectedMenu === 'Token' ? '#fff' : '#000' }]}>Likes</Text>
       </View>
 
       <TouchableOpacity onPress={() => handleMenuItem('Cart', 'Cart')}>
         <View style={homeStyles.cart1}>
-          <Icon name="cart" size={30} color={selectedMenu === 'Cart' ? '#fff' : '#000'} />
-          <Text style={[homeStyles.carts, { color: selectedMenu === 'Cart' ? '#fff' : '#000' }]}>Cart</Text>
+          <Icon name="ticket" size={30} color={selectedMenu === 'Cart' ? '#fff' : '#000'} />
+          <Text style={[homeStyles.carts, { color: selectedMenu === 'Cart' ? '#fff' : '#000' }]}>Orders</Text>
         </View>
       </TouchableOpacity>
 
       <TouchableOpacity onPress={() => handleMenuItem('Orders', 'Orders')}>
         <View style={homeStyles.menu1}>
-          <Icon name="menu" size={25} color={selectedMenu === 'Orders' ? '#fff' : '#000'} />
-          <Text style={[homeStyles.orders, { color: selectedMenu === 'Orders' ? '#fff' : '#000' }]}>Orders</Text>
+          <Icon  name="settings" size={25} color={selectedMenu === 'Orders' ? '#fff' : '#000'} />
+          <Text style={[homeStyles.orders, { color: selectedMenu === 'Orders' ? '#fff' : '#000' }]}>Setting</Text>
         </View>
       </TouchableOpacity>
 
